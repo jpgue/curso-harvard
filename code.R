3cbe588da2eb6901baccebd6512701fecadffed3
Juan Pablo Guerra Torres <jp.guerra20@outlook.com>
2020-10-23 11:15:34 -0500

    Unix

---
+++ code.R
@@ -0,0 +1,11 @@
+#ls #list dir content
+#mkdir folder_name #create directory called "folder_name"
+#rmdir folder_name  #remove an empty directory as long as it is empty
+#rm -r folder_name  #remove dir that is not empty, "r" stands for recursive
+#cd: change dir
+#../ # two dots represents parent dir
+  . # single dot represents current workingdir 
+#cd ~/projects # concatenate with forward slashes
+#cd ../.. # change to two parent layer beyond
+#cd -  # whatever dir you were before
+ # cd  # return to the home dir
